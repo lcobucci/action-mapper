0fb150c0ecb73b2413ce2951fb8e3a9e3d0acb0f
lcobucci <lcobucci@gmail.com>
2011-11-29 12:04:26 -0200

    Don't use $_REQUEST, but $_GET + $_POST

--- src/Lcobucci/ActionMapper/Http/Request.php
+++ src/Lcobucci/ActionMapper/Http/Request.php
@@ -47,7 +47,7 @@ class Request
 	public function getParams()
 	{
 		if (is_null($this->params)) {
-			$vars = $_REQUEST;
+			$vars = array_merget($_GET, $_POST);
 
 			if ($this->getMethod() == 'PUT' || $this->getMethod() == 'DELETE') {
 				$vars = array_merge($vars, $this->getStreamParams());
