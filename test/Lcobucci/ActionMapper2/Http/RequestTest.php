19b0acfae0864067821c83d9d403c204c143c1f5
Luís Otávio Cobucci Oblonczyk <lcobucci@gmail.com>
2012-06-20 14:49:12 -0300

    Fixes issue #9 by adding boot file and PHPUnit configuration file

---
+++ test/Lcobucci/ActionMapper2/Http/RequestTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace Lcobucci\ActionMapper2\Http;
+
+/**
+ * Request test case.
+ */
+class RequestTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function testing()
+    {
+        $this->assertTrue(true);
+    }
+}
