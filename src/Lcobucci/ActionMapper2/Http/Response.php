8e2aea0fe2acbed753376c52f7778eda38ba751d
Luís Otávio Cobucci Oblonczyk <lcobucci@gmail.com>
2012-06-22 12:50:16 -0300

    Use Symfony2 Http Foundation (#10)

--- src/Lcobucci/ActionMapper2/Http/Response.php
+++ src/Lcobucci/ActionMapper2/Http/Response.php
@@ -1,103 +1,60 @@
 <?php
 namespace Lcobucci\ActionMapper2\Http;
 
-use Lcobucci\ActionMapper2\Core\Application;
-
-class Response
+class Response extends \Symfony\Component\HttpFoundation\Response
 {
-	/**
-	 * @var \Lcobucci\ActionMapper2\Core\Application
-	 */
-	private $application;
+    /**
+     * @param string $content
+     */
+    public function appendContent($content)
+    {
+        return $this->setContent(
+            $this->getContent() . $content
+        );
+    }
 
-	/**
-	 * @var string
-	 */
-	private $content;
+    /**
+     * @param string $contentType
+     * @param string $charset
+     * @return \Lcobucci\ActionMapper2\Http\Response
+     */
+    public function setContentType($contentType, $charset = null)
+    {
+        if ($charset === null) {
+            $charset = $this->charset ?: 'UTF-8';
+        }
 
-	/**
-	 * @var array
-	 */
-	private $headers;
+        $this->headers->set(
+            'Content-Type',
+            $contentType . '; charset=' . $charset
+        );
 
-	/**
-	 * @param Application $application
-	 */
-	public function __construct(Application $application)
-	{
-		$this->application = $application;
-		$this->headers = array();
-		$this->content = '';
+        return $this;
+    }
 
-		$this->contentType(ContentType::HTML, 'UTF-8');
-	}
+    /**
+     * @param string $url
+     */
+    public function redirect($url)
+    {
+        $this->headers->set('Location', $url);
+    }
 
-	/**
-	 * @param string $content
-	 */
-	public function setContent($content)
-	{
-		$this->content = $content;
-	}
+    /**
+     * @see \Symfony\Component\HttpFoundation\Response::send()
+     */
+    public function send()
+    {
+        parent::send();
 
-	/**
-	 * @param string $content
-	 */
-	public function appendContent($content)
-	{
-		$this->content .= $content;
-	}
+        $this->terminateRequest();
+    }
 
-	/**
-	 * @param string $path
-	 */
-	public function redirect($path)
-	{
-		$this->headers[] = 'Location: ' . $this->application->getUrl() . $path;
-
-		$this->send();
-	}
-
-	public function contentType($type, $charset)
-	{
-		$this->headers[] = 'Content-Type: ' . $type . '; charset=' . $charset;
-	}
-
-	public function statusCode($status)
-	{
-		$this->headers[] = $_SERVER['SERVER_PROTOCOL'] . ' ' . $status;
-	}
-
-	/**
-	 * Sends the response headers
-	 */
-	protected function sendHeaders()
-	{
-		if (headers_sent()) {
-			return;
-		}
-
-		foreach ($this->headers as $header) {
-			header($header);
-		}
-	}
-
-	/**
-	 * Sends the response content
-	 */
-	protected function sendContent()
-	{
-		echo $this->content;
-	}
-
-	/**
-	 * Sends the application response
-	 */
-	public function send()
-	{
-		$this->sendHeaders();
-		$this->sendContent();
-
-		die();
-	}
+    /**
+     * Finishes the response
+     */
+    protected function terminateRequest()
+    {
+        exit();
+    }
 }
